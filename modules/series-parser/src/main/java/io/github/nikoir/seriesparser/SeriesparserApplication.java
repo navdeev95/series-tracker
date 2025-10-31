be84ffe5c3cd137a936f086acb4ae1208a804ddb
nikoir <n.avdeev95@gmail.com>
2025-10-31 14:14:35 +0700

    Структура проекта.

---
+++ modules/series-parser/src/main/java/io/github/nikoir/seriesparser/SeriesparserApplication.java
@@ -0,0 +1,13 @@
+package io.github.nikoir.seriesparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeriesparserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeriesparserApplication.class, args);
+	}
+
+}
---
+++ modules/series-parser/src/test/java/io/github/nikoir/seriesparser/SeriesparserApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.nikoir.seriesparser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeriesparserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
