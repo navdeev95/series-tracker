6b127118fc4efee7176d0cfe4834993989c4241c
nikoir <n.avdeev95@gmail.com>
2026-03-30 17:32:32 +0700

    * Расписание для scheduler-а.

--- src/main/java/io/github/nikoir/series/tracker/content/scheduler/ContentSyncScheduler.java
+++ src/main/java/io/github/nikoir/series/tracker/content/scheduler/ContentSyncScheduler.java
@@ -6,14 +6,12 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
-import java.util.concurrent.TimeUnit;
-
 @Service
 @RequiredArgsConstructor
 public class ContentSyncScheduler {
     private final SeriesSyncFacade syncFacade;
 
-    @Scheduled(fixedRate = 1, timeUnit = TimeUnit.HOURS)
+    @Scheduled(cron = "${sync.scheduler.cron:0 0 * * * *}")
     @Async
     public void syncContent() {
         syncFacade.syncAndNotifyAllSeries();
